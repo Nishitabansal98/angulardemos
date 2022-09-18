4d99466bd385ac3f63b448874064f51f2cb24116
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-09-18 22:46:09 +0530

    changes

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ap p-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })

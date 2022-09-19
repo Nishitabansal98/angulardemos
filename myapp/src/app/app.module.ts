beb44ef9a77289da73d3a1d08050452e07e6ce26
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-09-19 10:25:18 +0530

    changes

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { EcomComponent } from './ecom/ecom.component';
+import { CartComponent } from './ecom/cart/cart.component';
+import { EcomproductComponent } from './ecom/ecomproduct/ecomproduct.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
+    EcomComponent,
+    CartComponent,
+    EcomproductComponent,
   ],
   imports: [
     BrowserModule
---
+++ myapp/src/app/ecom/ecomproduct/ecomproduct.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ecomproduct',
+  templateUrl: './ecomproduct.component.html',
+  styleUrls: ['./ecomproduct.component.css']
+})
+export class EcomproductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

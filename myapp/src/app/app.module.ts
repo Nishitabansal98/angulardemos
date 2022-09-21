497199906e76e85f45e8892c14abeea3e5f46233
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-09-21 13:55:28 +0530

    changes

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
+    ProductsComponent,
   ],
   imports: [
     BrowserModule
